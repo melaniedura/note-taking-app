175e8357baa759620ad37c4a2f17595ba3c0a6bb
Melanie Durá Gil <melanie.159.dg@gmail.com>
2016-09-20 12:39:45 +0100

    create content provider

---
+++ app/src/main/java/com/melderomer/notes/NotesProvider.java
@@ -0,0 +1,63 @@
+package com.melderomer.notes;
+
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
+public class NotesProvider extends ContentProvider{
+
+    private static final String AUTHORITY = "com.melderomer.notes.notesprovider";
+    private static final String BASE_PATH = "notes";
+    public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + BASE_PATH );
+
+    private static final int NOTES = 1;
+    private static final int NOTES_ID = 2;
+
+    private static final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+    static {
+        uriMatcher.addURI(AUTHORITY, BASE_PATH, NOTES);
+        uriMatcher.addURI(AUTHORITY, BASE_PATH + "/#", NOTES_ID);
+    }
+
+    private SQLiteDatabase database;
+
+    @Override
+    public boolean onCreate() {
+        DBOpenHelper helper = new DBOpenHelper(getContext());
+        database = helper.getWritableDatabase();
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] strings, String s, String[] strings1, String s1) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String s, String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues contentValues, String s, String[] strings) {
+        return 0;
+    }
+}
