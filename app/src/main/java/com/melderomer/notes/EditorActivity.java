e901752df633beee8c92f12174f615dd09ddec45
Melanie Durá Gil <melanie.159.dg@gmail.com>
2016-09-21 12:09:31 +0100

    create new notes. customize floating action button.

--- app/src/main/java/com/melderomer/notes/EditorActivity.java
+++ app/src/main/java/com/melderomer/notes/EditorActivity.java
@@ -1,13 +1,21 @@
 package com.melderomer.notes;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 
 public class EditorActivity extends AppCompatActivity {
+
+    private String action;
+    private EditText editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,15 +24,53 @@ public class EditorActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        editor = (EditText) findViewById(R.id.editText);
+
+        Intent intent = getIntent();
+
+        Uri uri = intent.getParcelableExtra(NotesProvider.CONTENT_ITEM_TYPE);
+
+        if (uri == null) {
+            action = Intent.ACTION_INSERT;
+            setTitle(getString(R.string.new_note));
+        }
     }
 
+    private void finishEditing() {
+        String newText = editor.getText().toString().trim();
+
+        switch (action) {
+            case Intent.ACTION_INSERT:
+                if (newText.length() == 0) {
+                    setResult(RESULT_CANCELED);
+                } else {
+                    insertNote(newText);
+                }
+        }
+        finish();
+
+    }
+
+    private void insertNote(String noteText) {
+        ContentValues values = new ContentValues();
+        values.put(DBOpenHelper.NOTE_TEXT, noteText);
+        getContentResolver().insert(NotesProvider.CONTENT_URI, values);
+        setResult(RESULT_OK);
+    }
+
+    @Override
+    public void onBackPressed() {
+        finishEditing();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finishEditing();
+                break;
+        }
+        return true;
+    }
 }
